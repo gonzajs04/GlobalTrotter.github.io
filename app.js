afae667f82123c881c23915ac739e1d543f6f8ba
=Gonzalo Hernandez <Hernandezgonzalo584@gmail.com>
2023-04-24 21:48:38 -0300

    FIXED MQ

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
-document.addEventListener("DOMContentLoaded",iniciarApp);const desktop=window.matchMedia("(max-width:1280px");var swiper=new Swiper(".mySwiper",{slidesPerView:3,spaceBetween:30,freeMode:!0,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:2},1024:{slidesPerView:3}}});const containerNav=document.querySelector(".container-nav"),nav=document.querySelector(".nav"),btns=document.querySelector(".container-buttons");function iniciarApp(){desktop.addEventListener("change",()=>{handleScreenResize(desktop)}),controlarClickNav(),handleClickLinks()}function handleClickLinks(){let e=document.querySelectorAll(".smooth");e.forEach(e=>{e&&e.addEventListener("click",navegacionSmooth)})}function navegacionSmooth(e){e.preventDefault();let s=document.querySelector(e.target.attributes.href.value);containerNav.classList.remove("visible"),containerNav.classList.remove("display-visible"),nav.classList.remove("display-visible"),btns.classList.remove("display-visible"),s.scrollIntoView({behavior:"smooth"});let i=document.querySelector(".header");"100"==i.style.zIndex?i.style.zIndex="-1":i.style.zIndex="100"}function controlarClickNav(){let e=document.querySelector(".container-menu img");e&&e.addEventListener("click",()=>{desplegarMenu()})}function desplegarMenu(){if(containerNav&&(containerNav.classList.toggle("visible"),containerNav.classList.toggle("display-visible")),nav&&nav.classList.toggle("display-visible"),btns&&btns.classList.toggle("display-visible"),containerNav.classList.contains("visible")){let e=document.querySelector(".header");e.style.zIndex,e.style.zIndex="100"}}function handleScreenResize(e){e.matches?(document.querySelector(".container-map-access").style.order="1",document.querySelector(".container-suscribe-acesss").style.order="2"):(document.querySelector(".container-map-access").style.order="2",document.querySelector(".container-suscribe-acesss").style.order="1")}
+
+const header = document.querySelector('.header');
+
+document.addEventListener("DOMContentLoaded", iniciarApp);
+const desktop = window.matchMedia("(max-width:1280px");
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: !0,
+    pagination: { el: ".swiper-pagination", clickable: !0 },
+    breakpoints: { 0: { slidesPerView: 1 }, 768: { slidesPerView: 2 }, 1024: { slidesPerView: 3 } },
+});
+const containerNav = document.querySelector(".container-nav"),
+    nav = document.querySelector(".nav"),
+    btns = document.querySelector(".container-buttons");
+function iniciarApp() {
+    desktop.addEventListener("change", () => {
+        handleScreenResize(desktop);
+    }),
+        controlarClickNav(),
+        handleClickLinks();
+}
+function handleClickLinks() {
+    let e = document.querySelectorAll(".smooth");
+    e.forEach((e) => {
+        e && e.addEventListener("click", navegacionSmooth);
+    });
+}
+function navegacionSmooth(e) {
+    e.preventDefault();
+    let s = document.querySelector(e.target.attributes.href.value);
+    containerNav.classList.remove("visible"), containerNav.classList.remove("display-visible"), nav.classList.remove("display-visible"), btns.classList.remove("display-visible"), s.scrollIntoView({ behavior: "smooth" });
+
+    if(window.matchMedia('(max-width:1023)')){
+
+        
+    }
+      if(containerNav.classList.contains('visible')){
+  
+        header.style.zIndex=100;
+    }else{
+        header.style.zIndex=-1;
+    }
+}
+function controlarClickNav() {
+    let e = document.querySelector(".container-menu img");
+    e &&
+        e.addEventListener("click", () => {
+            desplegarMenu();
+        });
+}
+function desplegarMenu() {
+
+        (containerNav && (containerNav.classList.toggle("visible"), containerNav.classList.toggle("display-visible")),
+        nav && nav.classList.toggle("display-visible"),
+        btns && btns.classList.toggle("display-visible"),
+        containerNav.classList.contains("visible"))
+   
+    if(containerNav.classList.contains('visible')){
+  
+        header.style.zIndex=100;
+    }else{
+        header.style.zIndex=-1;
+    }
+   
+
+}
+function handleScreenResize(e) {
+    e.matches
+        ? ((document.querySelector(".container-map-access").style.order = "1"), (document.querySelector(".container-suscribe-acesss").style.order = "2"))
+        : ((document.querySelector(".container-map-access").style.order = "2"), (document.querySelector(".container-suscribe-acesss").style.order = "1"));
+}
